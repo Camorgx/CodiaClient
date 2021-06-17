1f23bd975ac18038b9c2be6b4aac061da609abdd
zzyyyl <zhengyile@mail.ustc.edu.cn>
2021-06-17 21:06:12 +0800

    Update UI

--- main.py
+++ main.py
@@ -10,14 +10,14 @@ def BeginLogin():
     loginusername = LoginUi.lineEdit.text()
     loginpassword = LoginUi.lineEdit_2.text()
     if not LoginUi.lineEdit_2.text():
-        QMessageBox.information(None,'登录失败','请输入密码',QMessageBox.Ok)
-    try:
-        client_login(loginusername,loginpassword)
-    except Error as e:
-        QMessageBox.information(None,'登录失败',str(e),QMessageBox.Ok)
+        QMessageBox.information(None, '登录失败', '请输入密码', QMessageBox.Ok)
     else:
-        loginusernickname = logined()
-        QMessageBox.information(None, '登录成功', '已登录用户' + loginusernickname, QMessageBox.Ok)
+        try: client_login(loginusername, loginpassword)
+        except Error as e:
+            QMessageBox.information(None, '登录失败', str(e), QMessageBox.Ok)
+        else:
+            loginusernickname = logined()
+            QMessageBox.information(None, '登录成功', '已登录用户: ' + loginusernickname, QMessageBox.Ok)
 def BeginReset():
     ResetUi.setupUi(ResetWindow)
     ResetWindowInit()
