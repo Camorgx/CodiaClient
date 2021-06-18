b16bb3dc292f876e821ec5d29149e3334a0e8f5d
曹高翔 <cgx@mail.ustc.edu.cn>
2021-06-18 08:13:48 +0800

    找回密码按钮行为更新

--- main.py
+++ main.py
@@ -9,18 +9,26 @@ from codiaclient import client_login
 def BeginLogin():
     loginusername = LoginUi.lineEdit.text()
     loginpassword = LoginUi.lineEdit_2.text()
+    if not LoginUi.lineEdit.text():
+        QMessageBox.information(None, '登录失败', '请输入邮箱或手机号', QMessageBox.Ok)
+        return False
     if not LoginUi.lineEdit_2.text():
         QMessageBox.information(None, '登录失败', '请输入密码', QMessageBox.Ok)
+        return False
     else:
-        try: client_login(loginusername, loginpassword)
+        try:
+            client_login(loginusername, loginpassword)
         except Error as e:
             QMessageBox.information(None, '登录失败', str(e), QMessageBox.Ok)
+            return False
         else:
             loginusernickname = logined()
             QMessageBox.information(None, '登录成功', '已登录用户: ' + loginusernickname, QMessageBox.Ok)
+            return True
 def BeginReset():
     ResetUi.setupUi(ResetWindow)
-    ResetWindowInit()
+    if ResetWindowInit():
+        ResetWindow.show()
 def GetCheck():
     if not ResetUi.alineEdit_Account.text():
         QMessageBox.information(None, '消息', '请输入邮箱或手机号。', QMessageBox.Ok)
@@ -28,7 +36,12 @@ def Reset():
     pass
 def ResetWindowInit():
     ResetUi.pushButtonGetCheck.clicked.connect(GetCheck)
-    ResetUi.pushButton_OK.clicked.connnect(Reset)
+    ResetUi.pushButton_OK.clicked.connect(Reset)
+    if not LoginUi.lineEdit.text():
+        QMessageBox.information(None, '消息', '请输入邮箱或手机号。', QMessageBox.Ok)
+        return False
+    ResetUi.alineEdit_Account.setText(LoginUi.lineEdit.text())
+    return True
 def BeginTask():
     LoginUi.pushButtonLogin.clicked.connect(BeginLogin)
     LoginUi.lineEdit_2.setEchoMode(QLineEdit.Password)
