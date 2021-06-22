5cc38ed7f32f9f7c68d3a5a4c1b2beaef9424033
zzyyyl <zhengyile@mail.ustc.edu.cn>
2021-06-22 11:13:34 +0800

    Modify codiaclientgui/utils.py

--- codiaclientgui/utils.py
+++ codiaclientgui/utils.py
@@ -12,7 +12,7 @@ Palette = {
 }
 import sys
 if sys.platform == 'win32': Font['main'].setFamily("Microsoft YaHei")
-elif sys.platform == 'darwin': Font['main'].setFamily(".AppleSystemUIFont")
+elif sys.platform == 'darwin': Font['main'].setFamily("SimSun")
 else: Font['main'].setFamily("SimSun")
 Font['main'].setPointSize(10)
 
