0636b75d1c36434c95704eb5ed4743a4e14135e1
曹高翔 <cgx@mail.ustc.edu.cn>
2021-06-22 22:00:23 +0800

    添加按钮功能

--- MainFunctions.py
+++ MainFunctions.py
@@ -10,12 +10,20 @@ import functionWindow
 from codiaclientgui.utils import Font, Palette
 from codiaclient.network import get_pack, show_pack
 
-pages = 0
+page_number = 0
+is_last_page = False
+last_pack_pid = None
+pages = []
+max_page_number = 1
+has_next = True
+last_page_number = -1
 
 
 def functionWindow_init(ui: functionWindow.Ui_functionWindow, nickname="UNDEFINED", verified=True):
-    global pages
-    pack_list = get_pack()
+    global page_number, is_last_page
+    global last_pack_pid
+    pack_list = get_pack()['nodes']
+    pages.append(pack_list)
     ui.exerciseFrame.hide()
     ui.packFrame.show()
     if verified:
@@ -27,19 +35,54 @@ def functionWindow_init(ui: functionWindow.Ui_functionWindow, nickname="UNDEFINE
     ui.statusbar.addWidget(status_bar_label)
     for dic in pack_list:
         add_item_to_pack_list(ui.listWidget_packs, dic)
-    pages = 1
+    page_number = 1
+    last_pack_pid = pack_list[0]['id']
     ui.pushButton_last.setEnabled(False)
-    ui.pushButton_next.clicked.connect(nextPage)
-    ui.pushButton_last.clicked.connect(lastPage)
+    ui.pushButton_next.clicked.connect(lambda: nextPage(ui))
+    ui.pushButton_last.clicked.connect(lambda: lastPage(ui))
 
 
-def nextPage():
-    pass
+def nextPage(ui: functionWindow.Ui_functionWindow):
+    global page_number, pages, max_page_number, last_pack_pid, has_next
+    global last_page_number
+    page_number += 1
+    if page_number > max_page_number:
+        pack_list = get_pack(before=last_pack_pid)
+        has_next = pack_list['pageInfo']['hasPreviousPage']
+        pack_list = pack_list['nodes']
+        last_pack_pid = pack_list[0]['id']
+        max_page_number += 1
+        if not has_next:
+            last_page_number = page_number
+    else:
+        pack_list = pages[page_number - 1]
+        if page_number == last_page_number:
+            has_next = False
+    if pack_list not in pages:
+        pages.append(pack_list)
+    if not has_next:
+        ui.pushButton_next.setEnabled(False)
+    ui.pushButton_last.setEnabled(True)
+    ui.label_page.setText('第 ' + str(page_number) + ' 页')
+    for i in range(0, ui.listWidget_packs.count()):
+        ui.listWidget_packs.takeItem(0)
+    for dic in pack_list:
+        add_item_to_pack_list(ui.listWidget_packs, dic)
 
 
-def lastPage():
-    global pages
-    pages -= 1
+def lastPage(ui: functionWindow.Ui_functionWindow):
+    global page_number, pages, has_next
+    page_number -= 1
+    has_next = True
+    if page_number == 1:
+        ui.pushButton_last.setEnabled(False)
+    ui.pushButton_next.setEnabled(True)
+    pack_list = pages[page_number - 1]
+    ui.label_page.setText('第 ' + str(page_number) + ' 页')
+    for i in range(0, ui.listWidget_packs.count()):
+        ui.listWidget_packs.takeItem(0)
+    for dic in pack_list:
+        add_item_to_pack_list(ui.listWidget_packs, dic)
 
 
 def get_pack_widget(data: dict):
