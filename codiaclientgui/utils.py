e412940f9fa41c5dc8dbac4577b1eaa95784b76e
zzyyyl <zhengyile@mail.ustc.edu.cn>
2021-06-25 09:40:39 +0800

    Modify codiaclientgui/utils.py, loginWindow.py, loginWindow.ui, mainWindow.py, mainWindow.ui

--- codiaclientgui/utils.py
+++ codiaclientgui/utils.py
@@ -18,6 +18,58 @@ Style = {
     "progressBar": "",
 }
 
+class _NewPushButton(QLabel):
+    clicked = pyqtSignal()
+    nowColor = QColor(241, 242, 243)
+    def setColor(self, col):
+        palette = self.palette()
+        palette.setBrush(QPalette.Active, QPalette.Background, QBrush(col))
+        self.setPalette(palette)
+        self.nowColor = col
+    color = pyqtProperty(QColor, fset=setColor)
+
+    def __init__(self, parent):
+        super(_NewPushButton, self).__init__(parent)
+        self.setStyleSheet("_NewPushButton { border: 1px solid #717273; border-radius: 5px } _NewPushButton:hover { border: 1px solid blue } NewPushButton:disabled { background-color: #f1f2f3 }")
+        self.setAlignment(Qt.AlignHCenter | Qt.AlignVCenter)
+
+    def enterEvent(self, e):
+        # print("enterEvent")
+        if not self.isEnabled(): return
+        self.setAutoFillBackground(True)
+        self.enterAnime = QPropertyAnimation(self, b"color")
+        self.enterAnime.setDuration(400)
+        self.enterAnime.setStartValue(self.nowColor)
+        self.enterAnime.setEndValue(QColor(227, 240, 255))
+        self.enterAnime.setEasingCurve(QEasingCurve.OutCubic)
+        self.enterAnime.start()
+
+    def leaveEvent(self, e):
+        # print("leaveEvent")
+        if not self.isEnabled(): return
+        self.setAutoFillBackground(True)
+        self.leaveAnime = QPropertyAnimation(self, b"color")
+        self.leaveAnime.setDuration(400)
+        self.leaveAnime.setStartValue(self.nowColor)
+        self.leaveAnime.setEndValue(QColor(241, 242, 243))
+        self.leaveAnime.setEasingCurve(QEasingCurve.OutCubic)
+        self.leaveAnime.start()
+
+    def mouseReleaseEvent(self, e):
+        # print("mouseReleaseEvent")
+        if 0 <= e.x() <= self.width() and 0 <= e.y() <= self.height():
+            self.clicked.emit()
+            self.enterEvent(e)
+
+    def mousePressEvent(self, e):
+        self.setAutoFillBackground(True)
+        self.pressedAnime = QPropertyAnimation(self, b"color")
+        self.pressedAnime.setDuration(400)
+        self.pressedAnime.setStartValue(self.nowColor)
+        self.pressedAnime.setEndValue(QColor(207, 220, 235))
+        self.pressedAnime.setEasingCurve(QEasingCurve.OutCubic)
+        self.pressedAnime.start()
+
 import sys
 if sys.platform == 'win32':
     Font['main'].setFamily("Microsoft YaHei")
@@ -25,11 +77,13 @@ if sys.platform == 'win32':
     Font['status'].setFamily("KaiTi")
     Font['status'].setPointSize(10)
     Style['progressBar'] = "QProgressBar { border: 1px solid grey; border-radius: 2px; text-align: center; background-color: #FFFFFF;}QProgressBar::chunk { background-color: #30A132; width: 10px;}"
+    NewPushButton = _NewPushButton
 elif sys.platform == 'darwin':
     Font['main'].setFamily(".AppleSystemUIFont")
     Font['main'].setPointSize(13)
     Font['status'].setFamily(".AppleSystemUIFont")
     Font['status'].setPointSize(13)
+    NewPushButton = QPushButton
 else:
     Font['main'].setFamily("Microsoft YaHei")
     Font['main'].setPointSize(13)
@@ -63,67 +117,3 @@ Palette['gray'].setBrush(QPalette.Inactive, QPalette.Text, grayBrush)
 grayBrush = QBrush(QColor(120, 120, 120))
 grayBrush.setStyle(Qt.SolidPattern)
 Palette['gray'].setBrush(QPalette.Disabled, QPalette.Text, grayBrush)
-
-class NewPushButton(QLabel):
-    mouseHovered = pyqtSignal()
-    mouseLeft = pyqtSignal()
-    clicked = pyqtSignal()
-    nowColor = QColor(241, 242, 243)
-    def setColor(self, col):
-        palette = self.palette()
-        palette.setColor(QPalette.Background, col)
-        self.setPalette(palette)
-        self.nowColor = col
-    color = pyqtProperty(QColor, fset=setColor)
-
-    def __init__(self, parent):
-        super(NewPushButton, self).__init__(parent)
-        # self.setFrameShadow(QFrame.Raised)
-        # self.setFrameShape(QFrame.StyledPanel)
-        self.setStyleSheet("NewPushButton { border: 1px solid gray; border-radius: 5px } NewPushButton::hover { border: 1px solid blue }")
-        self.setAlignment(Qt.AlignHCenter | Qt.AlignVCenter)
-        self.mouseHovered.connect(self.hoveredAnimeStart)
-        self.mouseLeft.connect(self.leftAnimeStart)
-
-    def hoveredAnimeStart(self):
-        if not self.isEnabled(): return
-        self.setAutoFillBackground(True)
-        self.hoveredAnime = QPropertyAnimation(self, b"color")
-        self.hoveredAnime.setDuration(400)
-        self.hoveredAnime.setStartValue(self.nowColor)
-        self.hoveredAnime.setEndValue(QColor(227, 240, 255))
-        self.hoveredAnime.setEasingCurve(QEasingCurve.OutCubic)
-        self.hoveredAnime.start()
-
-    def leftAnimeStart(self):
-        if not self.isEnabled(): return
-        self.setAutoFillBackground(True)
-        self.leftAnime = QPropertyAnimation(self, b"color")
-        self.leftAnime.setDuration(400)
-        self.leftAnime.setStartValue(self.nowColor)
-        self.leftAnime.setEndValue(QColor(241, 242, 243))
-        self.leftAnime.setEasingCurve(QEasingCurve.OutCubic)
-        self.leftAnime.start()
-
-    def enterEvent(self, e):
-        # print("enterEvent")
-        self.mouseHovered.emit()
-
-    def leaveEvent(self, e):
-        # print("leaveEvent")
-        self.mouseLeft.emit()
-
-    def mouseReleaseEvent(self, e):
-        # print("mouseReleaseEvent")
-        if 0 <= e.x() <= self.width() and 0 <= e.y() <= self.height():
-            self.clicked.emit()
-        self.mouseHovered.emit()
-
-    def mousePressEvent(self, e):
-        self.setAutoFillBackground(True)
-        self.pressedAnime = QPropertyAnimation(self, b"color")
-        self.pressedAnime.setDuration(400)
-        self.pressedAnime.setStartValue(self.nowColor)
-        self.pressedAnime.setEndValue(QColor(207, 220, 235))
-        self.pressedAnime.setEasingCurve(QEasingCurve.OutCubic)
-        self.pressedAnime.start()
